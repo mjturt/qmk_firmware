d0871931b8e7d121e545b094b76d2433a12d7d1f
mjturt <maks.turtiainen@protonmail.com>
2018-11-24 06:07:17 +0200

    Added flash script

--- keyboards/amj40/keymaps/mjturt/keymap.c
+++ keyboards/amj40/keymaps/mjturt/keymap.c
@@ -19,20 +19,29 @@
 
 #endif
 
+/****** Plans:
+ * alt position
+ * space/esc/tab modifier
+ * shift/viiva
+ * gui+numberlayer
+ * full row number
+ * raise and lower tap to ' or ~ for example
+ */
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    // 0: default
    KEYMAP(
          KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
          KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
-         KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT, 
+         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, 
          KC_LCTL, KC_LGUI, KC_LALT, MO(3), MO(1), KC_SPC, MO(2), KC_RALT, KC_RGUI, KC_RCTL),
 
    // 1: special characters
    KEYMAP(
          KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, 
          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RALT(KC_7), RALT(KC_0), KC_TRNS, KC_SLSH, KC_TRNS, 
+         KC_TRNS, RALT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RALT(KC_7), RALT(KC_0), KC_TRNS, KC_SLSH, KC_TRNS, 
          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
    // 2: numbers
