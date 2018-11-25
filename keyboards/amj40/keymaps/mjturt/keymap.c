0680d68c7323d993b0e255029200c176d2435157
mjturt <maks.turtiainen@protonmail.com>
2018-11-25 04:29:33 +0200

    amj40 mjturt keymap

--- keyboards/amj40/keymaps/mjturt/keymap.c
+++ keyboards/amj40/keymaps/mjturt/keymap.c
@@ -1,10 +1,6 @@
 #include "amj40.h"
-
-#ifndef KB_H
-#define KB_H
-
 #include "quantum.h"
-
+// Custom layout
 #define KEYMAP( \
       K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
       K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
@@ -17,100 +13,232 @@
       { K300,  K301,  K302,  K303,  K304,  K305,  KC_NO, KC_NO, K308,  K309,  K310,  K311 }  \
 }
 
-#endif
+// Layer definitions
+#define DEF 0
+#define SPC 1
+#define NUM 2
+#define NAV 3
+#define FUN 4
+#define NUP 5
+#define GAM 6
 
-/****** Plans:
- * alt position
- * space/esc/tab modifier
- * shift/viiva
- * gui+numberlayer
- * full row number
- * raise and lower tap to ' or ~ for example
- */
-
+// Keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-   // 0: default
-   KEYMAP(
-         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-         KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
-         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, 
-         KC_LCTL, KC_LGUI, KC_LALT, MO(3), MO(1), KC_SPC, MO(2), KC_RALT, KC_RGUI, KC_RCTL),
+   // 0: Default
+   [DEF] = KEYMAP(
+         LT(NUP, KC_TAB), KC_Q,    KC_W,             KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSPC, 
+         LT(NAV, KC_ESC), KC_A,    KC_S,             KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT, 
+         KC_LSFT,         KC_Z,    KC_X,             KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    LT(NAV,KC_ENT), 
+         KC_LCTL,         KC_LGUI, KC_LEAD, KC_LALT, MO(SPC),          LT(FUN, KC_SPC),          MO(NUM), KC_RALT, RALT(KC_2), RSFT_T(KC_BSLS)),
 
-   // 1: special characters
-   KEYMAP(
-         KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, 
-         KC_TRNS, RALT(KC_NUBS), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RALT(KC_7), RALT(KC_0), KC_TRNS, KC_SLSH, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+   // 1: Special characters
+   [SPC] = KEYMAP(
+         KC_TILD, KC_EXLM,       KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC,       KC_AMPR,    KC_ASTR,    KC_LPRN,       KC_RPRN, _______, 
+         _______, KC_RBRC,       _______, _______, _______, RALT(KC_4), RALT(KC_RBRC), KC_ASTR,    KC_LPRN,    RALT(KC_MINS), KC_RCBR, KC_PIPE, 
+         _______, KC_PLUS,       KC_EQL,  KC_LBRC, _______, _______,    _______,       RALT(KC_8), RALT(KC_9), KC_MINS,       KC_UNDS, _______, 
+         _______, _______,       _______, _______, _______,                 _______,               _______,    _______,       _______, _______),
 
-   // 2: numbers
-   KEYMAP(
-         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, 
-         KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, 
-         KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, RALT(KC_8), RALT(KC_9), KC_TRNS, KC_TRNS, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+   // 2: Numbers
+   [NUM] = KEYMAP(
+         KC_GRV,  KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, 
+         _______, _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+         _______, RALT(KC_NUBS), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+         _______, _______,       _______, _______, _______,          _______,          _______, _______, _______, _______),
 
-   // 3: controls, arrows(jklö)
-   KEYMAP(
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_HOME, KC_ESC, KC_END, KC_PSCR, KC_DEL, 
-         KC_TRNS, TG(4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, 
-         KC_TRNS, BL_TOGG, BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_TRNS, KC_VOLU, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT),
+   // 3: Navigation, arrows(jklö)
+   [NAV] = KEYMAP(
+         TG(NUP), _______, _______, _______, KC_PSCR, _______, KC_PGUP, KC_HOME, KC_ESC,  KC_END,  KC_BSPC, KC_DEL, 
+         _______, TG(GAM), _______, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, 
+         _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, KC_MUTE, _______, KC_VOLU, _______, 
+         _______, _______, _______, _______, _______,            KC_MPLY,        _______, KC_MPRV, KC_VOLD, KC_MNXT),
 
-   // 4: arrow layer for some games (toggle)
-   KEYMAP(
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, 
-         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT)
+   // 4: Function keys
+   [FUN] = KEYMAP(
+         KC_GRV,  KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,      KC_F8,       KC_F9,   KC_F10,  _______, 
+         _______, KC_F11,        KC_F12,  _______, _______, _______, _______, RALT(KC_7), RALT(KC_0),  _______, _______, _______, 
+         _______, RALT(KC_NUBS), _______, _______, _______, _______, _______, KC_NUBS,    S(KC_NUBS),  _______, _______, _______, 
+         _______, _______,       _______, _______, _______,          _______,             _______,     _______, _______, _______),
+
+   // 5: Numpad
+   [NUP] = KEYMAP(
+         _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9,    KC_MINS, KC_SLSH, _______, 
+         _______, _______, _______, _______, _______, _______, KC_4, KC_5, KC_6,    KC_PIPE, KC_AMPR, _______, 
+         _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3,    _______, KC_COMM, _______, 
+         _______, _______, _______, _______, _______,          KC_0,       KC_RPRN, _______, _______, _______),
+
+   // 6: Arrow layer for some games
+   [GAM] = KEYMAP(
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, 
+         _______, _______, _______, _______, _______,          _______,          _______, KC_LEFT, KC_DOWN, KC_RGHT),
 
 };
 
+/*
+ * Empty macro function
+ */
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
    return MACRO_NONE;
 }
 
+/*
+ * Empty init function
+ */
 void matrix_init_user(void) {
 }
 
+/*
+ * Scan function. Has leader key and led stuff.
+ */
+LEADER_EXTERNS();
+bool has_layer_changed = true;
+
 void matrix_scan_user(void) {
+
+   /*
+    * Leader key macros
+    */
+   LEADER_DICTIONARY() {
+      leading = false;
+      leader_end();
+      // VM workspace changing
+      SEQ_ONE_KEY(KC_Q) {
+         SEND_STRING(SS_LGUI("1"));
+      }
+      SEQ_ONE_KEY(KC_W) {
+         SEND_STRING(SS_LGUI("2"));
+      }
+      SEQ_ONE_KEY(KC_E) {
+         SEND_STRING(SS_LGUI("3"));
+      }
+      SEQ_ONE_KEY(KC_R) {
+         SEND_STRING(SS_LGUI("4"));
+      }
+      SEQ_ONE_KEY(KC_T) {
+         SEND_STRING(SS_LGUI("5"));
+      }
+      // IP
+      SEQ_TWO_KEYS(KC_I, KC_P) {
+         SEND_STRING("192.168.8.101");
+      }
+   }
+
+   /*
+    * Caps Lock led on when non-default layer is active
+    * AMJ40 caps lock led is under ESC in my layout
+    */
+   uint8_t layer = biton32(layer_state);
+   static uint8_t old_layer = 0;
+
+   if (old_layer != layer) {
+      has_layer_changed = true;
+      old_layer = layer;
+   }
+
+   if (has_layer_changed) {
+      has_layer_changed = false;
+
+      switch (layer) {
+         case SPC:
+            DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+            break;
+         case NUM:
+            DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+            break;
+         case NAV:
+            DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+            break;
+         case FUN:
+            DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+            break;
+         case NUP:
+            DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+            break;
+         case GAM:
+            DDRB |= (1 << 2); PORTB &= ~(1 << 2);
+            break;
+         case DEF:
+            DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
+            break;
+      }
+   }
 }
 
+/*
+ * Key press processing. Nothing at the moment.
+ */
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-   return true;
+   switch (keycode) {
+      case KC_A:
+         if (record->event.pressed) {
+         }
+         return true;
+      case KC_S:
+         if (record->event.pressed) {
+         }
+      default:
+         return true;
+   }
 }
 
+ /*
+  * For future RGB led stuff. For now just controls backlighting brightness.
+  */
+uint32_t layer_state_set_user(uint32_t state) {
+   switch (biton32(state)) {
+      case SPC:
+         //rgblight_setrgb (0x00,  0x00, 0xFF);
+         break;
+      case NUM:
+         //rgblight_setrgb (0xFF,  0x00, 0x00);
+         break;
+      case NAV:
+         //rgblight_setrgb (0x00,  0xFF, 0x00);
+         break;
+      case FUN:
+         //rgblight_setrgb (0x7A,  0x00, 0xFF);
+         break;
+      case NUP:
+         //rgblight_setrgb (0x7A,  0x00, 0xFF);
+         backlight_set(2);
+         break;
+      case GAM:
+         //rgblight_setrgb (0x7A,  0x00, 0xFF);
+         backlight_set(2);
+         break;
+      default:
+         backlight_set(4);
+         //rgblight_setrgb (0x00,  0xFF, 0xFF);
+         break;
+   }
+   return state;
+}
+
+/*
+ * Lock keys and leds, does nothing at the moment
+ */
 void led_set_user(uint8_t usb_led) {
 
    if (usb_led & (1 << USB_LED_NUM_LOCK)) {
-
    } else {
-
    }
 
    if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-      DDRB |= (1 << 2); PORTB &= ~(1 << 2);
    } else {
-      DDRB &= ~(1 << 2); PORTB &= ~(1 << 2);
    }
 
    if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
-
    } else {
-
    }
 
    if (usb_led & (1 << USB_LED_COMPOSE)) {
-
    } else {
-
    }
 
    if (usb_led & (1 << USB_LED_KANA)) {
-
    } else {
-
    }
 
 }
