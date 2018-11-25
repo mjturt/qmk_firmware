ed76d3dc6cfca702511a1eb7671ede51e38f6c60
mjturt <maks.turtiainen@protonmail.com>
2018-11-25 19:17:58 +0200

    Small changes

--- keyboards/amj40/keymaps/mjturt/keymap.c
+++ keyboards/amj40/keymaps/mjturt/keymap.c
@@ -1,19 +1,14 @@
+/*
+ * AMJ40 mjturt keymap.c
+ */
+
 #include "amj40.h"
 #include "quantum.h"
-// Custom layout
-#define KEYMAP( \
-      K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
-      K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
-      K200, K201, K202, K203, K204, K205, K206, K207, K208, K209, K210, K211, \
-      K300, K301, K302, K303, K304, K305,             K308, K309, K310, K311  \
-) { \
-      { K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009,  K010,  K011 }, \
-      { K100,  K101,  K102,  K103,  K104,  K105,  K106,  K107,  K108,  K109,  K110,  K111 }, \
-      { K200,  K201,  K202,  K203,  K204,  K205,  K206,  K207,  K208,  K209,  K210,  K211 }, \
-      { K300,  K301,  K302,  K303,  K304,  K305,  KC_NO, KC_NO, K308,  K309,  K310,  K311 }  \
-}
+#include "config.h"
 
-// Layer definitions
+/*
+ * Layer definitions
+ */
 #define DEF 0
 #define SPC 1
 #define NUM 2
@@ -22,7 +17,9 @@
 #define NUP 5
 #define GAM 6
 
-// Keymaps
+/*
+ * Keymaps
+ */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    // 0: Default
