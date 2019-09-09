546567c1196def30edb49144d07419977e7f1c32
mjturt <maks.turtiainen@gmail.com>
2019-09-09 23:24:17 +0300

    Fixed some errors

--- keyboards/jj50/keymaps/mjturt/keymap.c
+++ keyboards/jj50/keymaps/mjturt/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB_NAV, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSPC, \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    ENT_NAV, \
-        KC_LCTL, KC_LGUI, KC_LEAD, KC_LALT, MO(RAISE),KC_SPC,KC_SPC,MO(LOWER),  KC_RALT, KC_RGUI, RALT(KC_2), RSFT_T(KC_BSLS) \
+        KC_LCTL, KC_LGUI, _______, KC_LALT, MO(RAISE),KC_SPC,KC_SPC,MO(LOWER),  KC_RALT, KC_RGUI, RALT(KC_2), RSFT_T(KC_BSLS) \
     ),
 
     /* Lower
@@ -90,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [LOWER] = LAYOUT( \
-       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,  \
-       _______, _______, _______, _______, _______, _______, _______, RALT(KC_8), RALT(KC_9), _______, _______, _______,  \
-       _______, RALT(KC_NUBS), _______, _______, _______, _______, _______, KC_NUBS, S(KC_NUBS), _______, _______, _______,  \
-       RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
+       KC_F1,   KC_F2,         KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,      KC_F9,      KC_F10,  KC_F11,  KC_F12,   \
+       KC_GRV,  KC_1,          KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,       KC_8,       KC_9,    KC_0,    _______,  \
+       _______, _______,       _______, _______, _______, _______, _______, RALT(KC_8), RALT(KC_9), _______, _______, _______,  \
+       _______, RALT(KC_NUBS), _______, _______, _______, _______, _______, KC_NUBS,    S(KC_NUBS), _______, _______, _______,  \
+       RGB_TOG, _______,       _______, _______, _______, _______, _______, _______, _______,       _______, _______, _______   \
     ),
 
     /* Raise
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC,         KC_AMPR,    KC_ASTR,    KC_LPRN,       KC_RPRN, KC_DEL,  \
         _______, KC_RBRC, _______, _______, _______,   RALT(KC_4), RALT(KC_RBRC), KC_ASTR,    KC_LPRN,    RALT(KC_MINS), KC_RCBR, KC_PIPE, \
         _______, KC_PLUS, KC_EQL,  KC_LBRC, _______,   _______,    _______,       RALT(KC_7), RALT(KC_0), KC_MINS,       KC_UNDS, _______, \
-        BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     \
+        BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______     \
     ),
 
     /* Navigation
@@ -133,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [NAV] = LAYOUT( \
-       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, \
        _______, _______, _______, _______, KC_PSCR, KC_PGUP, _______, KC_HOME, KC_END,  KC_ESC,  KC_BSPC, KC_DEL,\
        _______, RGB_TOG, RGB_MOD, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_ENT,\
        _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, KC_MUTE, _______, KC_VOLU, _______, \
